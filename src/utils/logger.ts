5eab60edbef72058bc8a15d7be1ad72d94f5505e
Siddhartha Chatterjee <siddhartha.chatterjee@gmail.com>
2022-10-07 16:17:36 -0500

    Adding scaffold call.

---
+++ src/utils/logger.ts
@@ -0,0 +1,28 @@
+import winston from "winston";
+
+const levels = {
+    error: 0,
+    warn: 1,
+    info: 2,
+    http: 3,
+    debug: 4,
+};
+
+const level = () => {
+    const env = process.env.NODE_ENV ?? "development";
+    return env == "development" ? "debug" : "info";
+}
+
+const format = winston.format.combine(
+    winston.format.timestamp( { format: "YYYY-MM-DD HH:mm:ss:ms" }),
+    winston.format.printf((info) => `${info.timestamp}:${info.level} - ${info.message}`)
+);
+
+const transports = [new winston.transports.Console()];
+
+export const Logger = winston.createLogger({
+    level: level(),
+    levels,
+    format,
+    transports,
+});
